4d4f1c20f77d81d8088d207de3515b0b6c501ed9
eunag63 <77442567+eunag63@users.noreply.github.com>
2022-01-22 22:37:20 +0900

    ~ 06) 자료형 퀴즈

---
+++ grammar/sparta_java/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+
+        char c = 's';
+        int ten = 10;
+
+        System.out.println(c + "" + ten);
+
+        int[] thrl = new int[] {10,20,30};
+        System.out.println(thrl[thrl.length - 1]);
+
+    }
+}
