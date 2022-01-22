51abac19b907e5574e90da66db683a8709884ad2
eunag63 <77442567+eunag63@users.noreply.github.com>
2022-01-22 23:17:34 +0900

    ~ 11) 반복문 퀴즈

--- grammar/sparta_java/src/main/java/Main.java
+++ grammar/sparta_java/src/main/java/Main.java
@@ -2,14 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-
-        char c = 's';
-        int ten = 10;
-
-        System.out.println(c + "" + ten);
-
-        int[] thrl = new int[] {10,20,30};
-        System.out.println(thrl[thrl.length - 1]);
-
+        int i = 0;
+        int sum = 0;
+        while (i < 10){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
     }
 }
